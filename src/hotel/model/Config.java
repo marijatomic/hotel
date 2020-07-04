b0051b62167c7408b67385a068686874794ec297
Marija Tomic <marijat1995@gmail.com>
2020-07-04 13:28:22 +0200

    Create Config.java

---
+++ src/hotel/model/Config.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.model;
+
+public class Config {
+    public static final String HOSTNAME = "localhost";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    public static final String DATABASE = "hotel";
+    
+}
